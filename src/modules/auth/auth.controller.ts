731e99fd4cf68d3c879656aa0b89989f8387b91f
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-04-24 19:10:05 +0600

    - fix. refractor controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,21 +1,46 @@
 import { Request, Response, Router } from 'express'
+import AuthService from './auth.service'
 
-const appModule = Router()
+class AuthController {
+  private static readonly router = Router()
+  private static readonly authService: typeof AuthService = AuthService
 
-appModule.get('/auth/signup', async (_req: Request, res: Response) => {
-  res.send({
-    success: true,
-    code: 200,
-    // data,
-  })
-})
+  static get authModule() {
+    try {
+      const methods = Object.keys(this).splice(2) // skip constructor
 
-appModule.get('/auth/signin', (_req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+      methods.forEach((name) => eval(`this.${name}()`))
 
-appModule.get('/auth/signout', (_req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+      return this.router
+    } catch (error) {
+      console.log(`Auth module error: ${error}`)
+    }
+  }
 
-export default appModule
+  /* Hare are all the routes */
+  private static readonly signup = async (path = '/auth/signup') => {
+    this.authService.log()
+
+    this.router.get(path, async (_req: Request, res: Response) => {
+      res.send({
+        success: true,
+        code: 200,
+        // data,
+      })
+    })
+  }
+
+  private static readonly signin = async (path = '/auth/signin') => {
+    this.router.get(path, (_req: Request, res: Response) => {
+      res.send('Hello World!')
+    })
+  }
+
+  private static readonly signout = async (path = '/auth/signout') => {
+    this.router.get(path, (_req: Request, res: Response) => {
+      res.send('Hello World!')
+    })
+  }
+}
+
+export default AuthController.authModule
