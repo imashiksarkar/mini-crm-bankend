c1cffa98b2f6889567d7b8142ac8982bb607aaed
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-04-24 19:20:23 +0600

    - fix. auto skip to routes call

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,10 +4,12 @@ import AuthService from './auth.service'
 class AuthController {
   private static readonly router = Router()
   private static readonly authService: typeof AuthService = AuthService
+  private static readonly getPath = (path: string) => `/auth${path}`
 
   static get authModule() {
     try {
-      const methods = Object.keys(this).splice(2) // skip constructor
+      const EOFIndex = Object.keys(this).indexOf('EOF') + 1 || 0
+      const methods = Object.keys(this).splice(EOFIndex) // skip constructor
 
       methods.forEach((name) => eval(`this.${name}()`))
 
@@ -17,8 +19,10 @@ class AuthController {
     }
   }
 
+  private static readonly EOF = null // routes begin after line
+
   /* Hare are all the routes */
-  private static readonly signup = async (path = '/auth/signup') => {
+  private static readonly signup = async (path = this.getPath('/signup')) => {
     this.authService.log()
 
     this.router.get(path, async (_req: Request, res: Response) => {
@@ -30,13 +34,13 @@ class AuthController {
     })
   }
 
-  private static readonly signin = async (path = '/auth/signin') => {
+  private static readonly signin = async (path = this.getPath('/signin')) => {
     this.router.get(path, (_req: Request, res: Response) => {
       res.send('Hello World!')
     })
   }
 
-  private static readonly signout = async (path = '/auth/signout') => {
+  private static readonly signout = async (path = this.getPath('/signout')) => {
     this.router.get(path, (_req: Request, res: Response) => {
       res.send('Hello World!')
     })
