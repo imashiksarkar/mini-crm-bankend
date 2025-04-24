7efa6a49bc95935aaf651c1a29922d8fbeb6b45a
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-04-24 17:16:21 +0600

    - add tokens schema

--- backend/src/modules/auth/db/schema/index.ts
+++ backend/src/modules/auth/db/schema/index.ts
@@ -1,11 +1,31 @@
-import { pgEnum, pgTable, serial, text } from 'drizzle-orm/pg-core'
+import { pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const userRoleEnum = pgEnum('user_role', ['user', 'admin'])
 
 export const usersTable = pgTable('users', {
-  id: serial('id').primaryKey(),
-  fullName: text('full_name').notNull(),
+  id: uuid('id').primaryKey(),
+  name: text('name').notNull(),
   email: text('email').notNull(),
   password: text('password').notNull(),
   role: userRoleEnum('role').array().notNull().default(['user']),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+})
+
+export const tokensTable = pgTable('tokens', {
+  id: uuid('id').primaryKey(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  token: text('token').notNull(),
+  expiresAt: timestamp('expires_at').notNull(),
+  revokedAt: timestamp('revoked_at'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 })
